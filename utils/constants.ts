c6a273a7884e8758a6f04bd446f16ea9ce65be6e
s.aoi <104545731+SakiAoiaoi@users.noreply.github.com>
2025-12-06 10:33:28 +0900

    Update constants.ts

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,2 +1,2 @@
 // base sepoliaにデプロイしたシューティングゲームNFT
-export const NFT_ADDRESS = '0x159D90cFa7f5efF4c1b8EBBea18504A3a69BdDf1';
+export const NFT_ADDRESS = '0xBa776978480ae4F5adE4134D50806A6808b655D5';
